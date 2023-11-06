bac9de7d67d8c2f64e0078e940f1274917a75add
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-11-06 19:56:27 +0200

    Fixed application logic and resolved output issues

--- src/main/java/ua/foxminded/git/anagrams/AnagramMaker.java
+++ src/main/java/ua/foxminded/git/anagrams/AnagramMaker.java
@@ -1,42 +1,51 @@
 package ua.foxminded.git.anagrams;
 
+import java.util.StringJoiner;
+
 public class AnagramMaker {
-    private String sourceString;
+    private final String sourceString;
 
     public AnagramMaker(String sourceString) {
         this.sourceString = sourceString;
     }
 
-    private String reverseWord(String word) {
-        char[] charactersSource = word.toCharArray();
-        char[] charactersFinal = new char[charactersSource.length];
+    private String reverseOneWord(String word) {
+        char[] charSource = word.toCharArray();
+        char[] charFinal = new char[charSource.length];
+        int finalIndex = charSource.length - 1;
 
-        for (int i = 0; i < charactersSource.length; i++) {
-            if (Character.isLetter(charactersSource[i])) {
-                charactersFinal[charactersFinal.length - 1 - i] = charactersSource[i];
-            } else {
-                charactersFinal[i] = charactersSource[i];
+        for (int i = 0; i < charSource.length; i++) {
+            if (!(Character.isLetter(charSource[i]))) {
+                charFinal[i] = charSource[i];
             }
         }
-        return new String(charactersFinal);
+
+        for (int i = 0; i < charSource.length; i++) {
+            if (Character.isLetter(charSource[i])) {
+                // Checking if the character at charFinal[finalIndex] is a null character
+                if (charFinal[finalIndex] == '\u0000') {
+                    charFinal[finalIndex] = charSource[i];
+                } else {
+                    charFinal[finalIndex - 1] = charSource[i];
+                }
+                finalIndex--;
+            }
+        }
+
+        return new String(charFinal);
     }
 
-    private String reverseString(String sourceString) {
+    private String reverseAllWords(String sourceString) {
         String[] words = sourceString.split(" ");
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            stringBuilder.append(reverseWord(words[i]));
-            if (i < words.length - 1) {
-                stringBuilder.append(" ");
-            }
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        for (String word : words) {
+            stringJoiner.add(reverseOneWord(word));
         }
-        return stringBuilder.toString();
+        return stringJoiner.toString();
     }
 
-    public void anagramMaker() {
-        String result = reverseString(sourceString);
+    public void viewAnagramMaker() {
+        String result = reverseAllWords(sourceString);
         System.out.println(result);
     }
-
-
 }
--- src/main/java/ua/foxminded/git/anagrams/Main.java
+++ src/main/java/ua/foxminded/git/anagrams/Main.java
@@ -1,12 +1,16 @@
 package ua.foxminded.git.anagrams;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String testWords = "doom s1ayer bfg8000";
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
 
-        AnagramMaker maker = new AnagramMaker(testWords);
-        maker.anagramMaker();
+        AnagramMaker maker = new AnagramMaker(str);
+        maker.viewAnagramMaker();
 
     }
 }
