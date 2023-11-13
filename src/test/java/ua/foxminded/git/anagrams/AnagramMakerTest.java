0e6f7d001be2d37b2cc3d3aa3b989c35e410b6ba
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-11-13 15:11:12 +0200

    Fixed logic tests

--- src/test/java/ua/foxminded/git/anagrams/AnagramMakerTest.java
+++ src/test/java/ua/foxminded/git/anagrams/AnagramMakerTest.java
@@ -13,61 +13,98 @@ class AnagramMakerTest {
         IllegalArgumentException actualException = assertThrows(IllegalArgumentException.class, () -> {
             AnagramMaker maker = new AnagramMaker(null);
         });
+        String expectedException = "Source string cannot be null";
 
-        assertEquals("Source string cannot be null", actualException.getMessage());
+        assertEquals(expectedException, actualException.getMessage());
     }
 
     @Test
     void testAnagramMakerWithEmptyString() {
         AnagramMaker maker = new AnagramMaker("");
-        assertEquals("", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithSingleSpace() {
         AnagramMaker maker = new AnagramMaker("ja17va java17");
-        assertEquals("av17aj avaj17", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "av17aj avaj17";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithSeveralSpace() {
         AnagramMaker maker = new AnagramMaker("ja17va     java17");
-        assertEquals("av17aj     avaj17", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "av17aj     avaj17";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithSingleCharacter() {
         AnagramMaker maker = new AnagramMaker("@");
-        assertEquals("@", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "@";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithMultipleSameLetter() {
         AnagramMaker maker = new AnagramMaker("jjj");
-        assertEquals("jjj", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "jjj";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithSameCharacterInLowerAndUpperCasesMixed() {
         AnagramMaker maker = new AnagramMaker("AAaAaa");
-        assertEquals("aaAaAA", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "aaAaAA";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithWordWithDifferentLetters() {
         AnagramMaker maker = new AnagramMaker("jvm");
-        assertEquals("mvj", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "mvj";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithOnlySymbols() {
         AnagramMaker maker = new AnagramMaker("17!@#");
-        assertEquals("17!@#", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "17!@#";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testAnagramMakerWithSeveralWords() {
         AnagramMaker maker = new AnagramMaker("java maven junit");
-        assertEquals("avaj nevam tinuj", maker.resultAnagramMaker());
+
+        String actual = maker.resultAnagramMaker();
+        String expected = "avaj nevam tinuj";
+
+        assertEquals(expected, actual);
     }
 }
